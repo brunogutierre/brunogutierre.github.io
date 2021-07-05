6475870bff441bb20c65b63e39e2350b4bc16766
Bruno Gutierre <bruno.gutierre@serpro.gov.br>
2021-07-04 21:29:46 -0300

    front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FaceComponent } from './base/comps/face/face.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: './home', component: FaceComponent },
+
+  { path: '**', component: FaceComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
